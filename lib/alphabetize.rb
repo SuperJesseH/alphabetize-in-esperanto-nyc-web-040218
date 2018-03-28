dd012b73524fb5e502e30a40036727b7c4d45155
SuperJesseH <therealjhorwitz@gmail.com>
2018-03-28 01:05:52 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
+require 'pry'
+
 def alphabetize(arr)
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   # code here
+  arr.sort_by do |word|
+    word.split('').collect do |letter|
+      alphabet.index(letter)
+    end
+  end
 end
